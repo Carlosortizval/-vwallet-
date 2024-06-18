d4c38704f7f1f3b3e94c42f729248211d7fdf89f
Carlosortizval <carlospatricioortizvaldivia@gmail.com>
2024-06-18 13:44:06 -0400

    Create VwalletApplication.java

---
+++ VwalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.vwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VwalletApplication.class, args);
+    }
+
+}
